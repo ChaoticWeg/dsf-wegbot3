e312e37fffa62ca528c2c0c4ad6d4278817be978
ChaoticWeg <shawn@chaoticweg.cc>
2019-10-07 02:48:05 -0500

    try to react rather than respond, if possible

--- lib/cogs/test/testcog.py
+++ lib/cogs/test/testcog.py
@@ -1,4 +1,5 @@
-from discord.ext.commands import command, is_owner, guild_only
+from discord.ext.commands import Context, command, is_owner, guild_only
+from discord import utils as discord_utils, Emoji
 
 from ..base import WegbotCog
 
@@ -8,5 +9,9 @@ class TestCog(WegbotCog, name="Test", command_attrs=dict(hidden=True)):
     @is_owner()
     @guild_only()
     @command("test")
-    async def test(self, ctx):
-        await ctx.send(f"{ctx.author.mention}, all checks went ok")
+    async def test(self, ctx: Context):
+        ok_emoji: Emoji = discord_utils.get(ctx.guild.emojis, name="green_check")
+        if ok_emoji is not None:
+            await ctx.message.add_reaction(ok_emoji)
+        else:
+            await ctx.send(":+1:")
