56e949f6f036c8c7caccbe0e5983812ed4dc9c1d
ChaoticWeg <shawn@chaoticweg.cc>
2019-10-17 22:37:04 -0500

    require permission_requests table

--- lib/cogs/commands/meta/meta_permission_request.py
+++ lib/cogs/commands/meta/meta_permission_request.py
@@ -6,6 +6,7 @@ class MetaPermissionRequestCog(WegbotCog, name="MetaPermissionRequest", command_
 
     @commands.is_owner()
     @commands.guild_only()
+    @WegbotCog.has_table("permission_requests")
     @commands.group("pr", aliases=["fake-request"])
     async def cmd(self, ctx: commands.Context, permission_name: str):
         await self.bot.request_permission(ctx.channel, permission_name)
