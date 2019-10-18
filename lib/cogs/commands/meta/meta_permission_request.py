a9731872441c1b964f90c5ed53e0e386e7419afb
ChaoticWeg <shawn@chaoticweg.cc>
2019-10-17 22:19:51 -0500

    add command to manually trigger a fake permissions request

---
+++ lib/cogs/commands/meta/meta_permission_request.py
@@ -0,0 +1,11 @@
+from discord.ext import commands
+from lib.cogs.base import WegbotCog
+
+
+class MetaPermissionRequestCog(WegbotCog, name="MetaPermissionRequest", command_attrs=dict(hidden=True)):
+
+    @commands.is_owner()
+    @commands.guild_only()
+    @commands.group("pr", aliases=["fake-request"])
+    async def cmd(self, ctx: commands.Context, permission_name: str):
+        await self.bot.request_permission(ctx.channel, permission_name)
