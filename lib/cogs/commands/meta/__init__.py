3c6741ae06ace521aed4d94e51cf194f9400448f
ChaoticWeg <shawn@chaoticweg.cc>
2019-11-07 11:03:58 -0600

    implement MetaModsCog

--- lib/cogs/commands/meta/__init__.py
+++ lib/cogs/commands/meta/__init__.py
@@ -2,9 +2,11 @@ from lib.wegbot import Wegbot
 from .meta_admin_channel import MetaAdminChannelCog
 from .meta_key_value import MetaKeyValueCog
 from .meta_roles import MetaRolesCog
+from .meta_mods import MetaModsCog
 
 
 def setup(bot: Wegbot):
     bot.add_cog(MetaRolesCog(bot))
     bot.add_cog(MetaKeyValueCog(bot))
     bot.add_cog(MetaAdminChannelCog(bot))
+    bot.add_cog(MetaModsCog(bot))
--- lib/cogs/commands/meta/meta_mods.py
+++ lib/cogs/commands/meta/meta_mods.py
@@ -1,6 +1,7 @@
+import discord
 from discord.ext import commands
+
 from lib.cogs.base import WegbotCog
-from lib.errors.commands import CommandNotImplementedError
 
 
 class MetaModsCog(WegbotCog, name="MetaMods"):
@@ -8,7 +9,31 @@ class MetaModsCog(WegbotCog, name="MetaMods"):
 
     @WegbotCog.has_table("mods")
     @WegbotCog.is_mod()
-    @commands.is_owner()
+    @commands.guild_only()
     @commands.group("mods")
-    def cmd(self, ctx: commands.Context):
-        raise CommandNotImplementedError(ctx.command)
+    async def cmd(self, ctx: commands.Context):
+        """ List mods """
+        if ctx.invoked_subcommand is None:
+            await self.list.invoke(ctx)
+
+    @cmd.command("list")
+    async def list(self, ctx: commands.Context):
+        """ List mods """
+        mod_ids = self.db.mods.get_all(ctx.guild)
+        msg = f"there are {'no' if len(mod_ids) == 0 else len(mod_ids)} mods in this server" if not len(mod_ids) == 1 \
+            else "there is 1 mod in this server"
+        await ctx.send(f"{ctx.author.mention}, {msg}. due to API restrictions, i can't list mods at the moment.")
+
+    @cmd.command("add")
+    async def add(self, ctx: commands.Context, member: discord.Member):
+        """ Add mods by mentioning them """
+        if self.db.mods.is_mod(member):
+            await ctx.send(f"{ctx.author.mention}, `{member}` is already a mod.")
+            return
+
+        if member.bot:
+            await ctx.send(f"{ctx.author.mention}, `{member}` is a bot and cannot be a mod.")
+            return
+
+        self.db.mods.add(member)
+        await ctx.send(f"{ctx.author.mention}, i have added `{member}` as a mod.")
