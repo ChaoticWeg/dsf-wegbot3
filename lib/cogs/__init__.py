804108ae17493399370769a4986f7a7e65b68c1f
ChaoticWeg <shawn@chaoticweg.cc>
2019-11-11 22:26:13 -0600

    look for files as well as directories in lib.cogs

--- lib/cogs/__init__.py
+++ lib/cogs/__init__.py
@@ -1,13 +1,31 @@
 from pathlib import Path
 from os import path
 
-
 this_dir = Path(path.realpath(path.dirname(__file__)))
 commands_dir = this_dir / "commands"
 extensions_dir = this_dir / "extensions"
 
-ignored_cogs = ["__pycache__"]
+ignored_cogs = ["__pycache__", "__init__.py"]
 
-command_names = [f"cogs.commands.{n.name}" for n in commands_dir.iterdir() if n.is_dir() and n.name not in ignored_cogs]
-ext_names = [f"cogs.extensions.{n.name}" for n in extensions_dir.iterdir() if n.is_dir() and n.name not in ignored_cogs]
-cog_names = command_names + ext_names
+
+command_dirs = [f"cogs.commands.{n.name}"
+                for n in commands_dir.iterdir()
+                if n.is_dir()
+                and n.name not in ignored_cogs]
+
+command_files = [f"cogs.commands.{n.name[:-3]}"
+                 for n in commands_dir.iterdir()
+                 if n.is_file()
+                 and n.name not in ignored_cogs]
+
+ext_dirs = [f"cogs.extensions.{n.name}"
+            for n in extensions_dir.iterdir()
+            if n.is_dir()
+            and n.name not in ignored_cogs]
+
+ext_files = [f"cogs.extensions.{n.name[:-3]}"
+             for n in extensions_dir.iterdir()
+             if n.is_file()
+             and n.name not in ignored_cogs]
+
+cog_names = command_dirs + command_files + ext_dirs + ext_files
